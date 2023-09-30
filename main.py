d7cdc23247325d08c31304af79cf190671b85489
sahd11 <sahdipesh11@gmail.com>
2023-09-30 22:39:54 +1000

    Add toolbar in the window. Add icons for search and add actions.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, \
     QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, \
-    QTableWidgetItem, QDialog, QVBoxLayout, QComboBox
-from PyQt6.QtGui import QAction
+    QTableWidgetItem, QDialog, QVBoxLayout, QComboBox, QToolBar
+from PyQt6.QtGui import QAction, QIcon
 import sys
 import sqlite3
 
@@ -11,6 +11,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
 
         # Add menu items
         file_menu_item = self.menuBar().addMenu("&File")
@@ -18,7 +19,7 @@ class MainWindow(QMainWindow):
         edit_menu_item = self.menuBar().addMenu("&Edit")
 
         # Add sub-item (action) to the file menu.
-        add_student_action = QAction("Add student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add student", self)
         add_student_action.triggered.connect(self.insert) # Call insert method
         file_menu_item.addAction(add_student_action)
 
@@ -27,7 +28,7 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
 
         # Add action to the edit menu.
-        search_action = QAction("Search", self)
+        search_action = QAction(QIcon("icons/search.png"), "Search", self)
         edit_menu_item.addAction(search_action)
         search_action.triggered.connect(self.search)
 
@@ -37,6 +38,16 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(("id", "Name", "Course", "Mobile"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table) # Specify table as central widget.
+
+        # Create toolbar and toolbar elements.
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
+
+
 
     def load_date(self):
         # Create a connection to the database and read data from student table.
